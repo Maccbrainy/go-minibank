c1921a9e40c76ef4c480e0dbc9387e6bc3fa9bb2
Maccbrainy <iketakumichaelnedum@gmail.com>
2024-06-18 19:45:21 +0100

    program update

--- api.go
+++ api.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -9,7 +11,51 @@ import (
 
 type APIServer struct {
 	listenAddress string
+	store         Storage
 }
+
+func NewAPIServer(listenAddress string, store Storage) *APIServer {
+	return &APIServer{
+		listenAddress: listenAddress,
+		store:         store,
+	}
+}
+
+func (server *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(server.handleAccount))
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(server.handleGetAccount))
+
+	log.Println("JSON API Server running on port:", server.listenAddress)
+	http.ListenAndServe(server.listenAddress, router)
+}
+
+func (server *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "GET" {
+		return server.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return server.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return server.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("method not allowed %s", r.Method)
+}
+func (server *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	// account := NewAccount("Maccbrainy", "Michael")
+	id := mux.Vars(r)["id"]
+	fmt.Printf(id)
+	return WriteJSON(w, http.StatusOK, &Account{})
+}
+func (server *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (server *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (server *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error { return nil }
 
 type ApiError struct {
 	Error string
@@ -17,8 +63,8 @@ type ApiError struct {
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
 func WriteJSON(w http.ResponseWriter, status int, value any) error {
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(value)
 }
 
@@ -31,25 +77,3 @@ func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 		}
 	}
 }
-
-func NewAPIServer(listenAddress string) *APIServer {
-	return &APIServer{
-		listenAddress: listenAddress,
-	}
-}
-
-func (server *APIServer) Run() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/account", makeHTTPHandleFunc(server.handleAccount))
-}
-
-func (server *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error    { return nil }
-func (server *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error { return nil }
-func (server *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
-}
-func (server *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
-}
-func (server *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error { return nil }
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
+	store, err := NewPostgresStore()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	server := NewAPIServer(":5000", store)
+	server.Run()
 	fmt.Println("Viola!This is working")
 }
