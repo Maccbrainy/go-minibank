c94e5634b8c02f05fed98e9867128eac3117c13d
Maccbrainy <iketakumichaelnedum@gmail.com>
2024-06-20 05:58:40 +0100

    Account api handlers defined

--- api.go
+++ api.go
@@ -26,7 +26,7 @@ func (server *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(server.handleAccount))
-	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(server.handleGetAccount))
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(server.handleGetAccountByID))
 
 	log.Println("JSON API Server running on port:", server.listenAddress)
 	http.ListenAndServe(server.listenAddress, router)
@@ -44,22 +44,49 @@ func (server *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) e
 	}
 	return fmt.Errorf("method not allowed %s", r.Method)
 }
+
 func (server *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	// account := NewAccount("Maccbrainy", "Michael")
+	accounts, err := server.store.GetAccounts()
+	if err != nil {
+		return err
+	}
+	return WriteJSON(w, http.StatusOK, accounts)
+}
+func (server *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request) error {
+
+	//string id from param
 	id := mux.Vars(r)["id"]
-	fmt.Printf(id)
-	return WriteJSON(w, http.StatusOK, &Account{})
+	//Convert string to int
+	accountID, err := strconv.Atoi(id)
+	if err != nil {
+		return fmt.Errorf("Invalid Id %s", id)
+	}
+	account, err := server.store.GetAccountByID(accountID)
+	if err != nil {
+		return err
+	}
+	return WriteJSON(w, http.StatusOK, account)
 }
 func (server *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	createAccountRequest := new(CreateAccountRequest)
+	if err := json.NewDecoder(r.Body).Decode(createAccountRequest); err != nil {
+		return err
+	}
+
+	account := NewAccount(createAccountRequest.FirstName, createAccountRequest.LastName)
+	if err := server.store.CreateAccount(account); err != nil {
+		return nil
+	}
+	return WriteJSON(w, http.StatusOK, account)
 }
+
 func (server *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 func (server *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error { return nil }
 
 type ApiError struct {
-	Error string
+	Error string `json:"error"`
 }
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
